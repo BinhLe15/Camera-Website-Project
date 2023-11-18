146a62cb3ed05d66a348837c5bf2335c0328fd4d
BinhLe1505 <Binhnam1505@gmail.com>
2023-11-18 16:34:06 +0700

    firts commit

---
+++ src/componetns/layout/layout.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import "./layout.css"
+const { Header, Content, Footer } = Layout;
+const WebLayout = () => {
+  return (
+    <Layout>
+      <Header className='layoutHeader'>
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={new Array(3).fill(null).map((_, index) => ({
+            key: String(index + 1),
+            label: `nav ${index + 1}`,
+          }))}
+        />
+      </Header>
+      <Content
+        className="site-layout">
+        <Breadcrumb>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className='site-content'>
+          Content
+        </div>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        Ant Design ©2023 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+};
+export default WebLayout;
